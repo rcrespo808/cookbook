9f2cbff06f4c9fe90e8e9731695b11bc552ee669
Ramiro <rcrespo808@gmail.com>
2019-09-27 10:52:36 -0400

    Initial Commit

---
+++ src/main/java/com/cookbook/CookBook/CookBookApplication.java
@@ -0,0 +1,13 @@
+package com.cookbook.CookBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookBookApplication.class, args);
+	}
+
+}
